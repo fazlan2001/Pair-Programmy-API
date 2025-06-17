8581d4fdea575d2f6294f4f7d90594e758b7fe17
Fazlan <fazlanfowmy@gmail.com>
2025-06-17 18:55:45 +0530

    Updated WebSocketConfig to allow Netlify frontend origin

--- pairProgrammy/src/main/java/com/fazzy/pairProgrammy/config/WebSocketConfig.java
+++ pairProgrammy/src/main/java/com/fazzy/pairProgrammy/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("https://pair-programmy.netlify.app/")
                 .withSockJS();
     }
 
